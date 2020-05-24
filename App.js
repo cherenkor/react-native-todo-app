9fcb64b9b439b6a488a8467267ed4ff90551136d
hamsish <cherenkor@Sergeys-MacBook-Pro.local>
2020-05-24 19:37:16 +0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Navbar} from "./src/Navbar";
+import {AddTodo} from "./src/AddTodo";
+import {Todo} from "./src/Todo";
 
 export default function App() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = title => {
+        setTodos(prev => [
+            ...prev,
+            {
+                id: Date.now().toString(),
+                title
+            }
+        ])
+    }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Navbar title="Todo App" />
+        <View style={styles.container}>
+           <AddTodo onSubmit={addTodo} />
+           <View>
+               {
+                   todos.map(({ id, title }) => (
+                       <Todo key={id} title={title} />
+                   ))
+               }
+           </View>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      paddingHorizontal: 30,
+      paddingVertical: 20
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple TODO app on React Native (Expo)
+
+# Run
+```$xslt
+npm i
+npm run start
+```
