b8167e69afcaebfc820bedfb88b039b17c9946ba
cherenkor <buhanka1994@gmail.com>
2020-05-30 21:57:11 +0300

    Add no todos image

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,17 +1,44 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import { AddTodo } from "../components/AddTodo";
 import { TodoList } from "../components/TodoList";
 
 const MainScreen = ({ todos, addTodo, removeTodo, setTodoId }) => {
+  let content = (
+    <TodoList todos={todos} removeTodo={removeTodo} setTodoId={setTodoId} />
+  );
+
+  if (todos.length === 0) {
+    content = (
+      <View style={styles.imgWrap}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/no-items.png")}
+        />
+      </View>
+    );
+  }
+
   return (
     <View>
       <AddTodo todos={todos} addTodo={addTodo} />
-      <TodoList todos={todos} removeTodo={removeTodo} setTodoId={setTodoId} />
+      {content}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imgWrap: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 300,
+    padding: 10,
+  },
+  image: {
+    resizeMode: "contain",
+    height: "100%",
+    width: "100%",
+  },
+});
 
 export default MainScreen;
